86bff106eb18903d6fd5a57f8d2353f5a27c6066
Jorge Ruiz <201914@CALASANZSALAMANCA.LOCAL>
2021-09-29 11:34:19 +0200

    [ADD] Ejercicios JS

---
+++ Desarrollo web en entorno clientge/empezar con js/1/js/js.js
@@ -0,0 +1,6 @@
+function uno(){
+ alert("hola fqr4qrrt");
+}
+function mostrarVariables(){
+    alert('Variable1: ' + variable1); 
+}
---
+++ Desarrollo web en entorno clientge/empezar con js/2/js/js.js
@@ -0,0 +1,15 @@
+
+function uno(){
+    alert("hola fqr4qrrt");
+   }
+   function mostrarVariables(){
+    var  variable1=34;
+       alert('Variable1: ' + variable1); 
+     
+   }
+ 
+   function multi(){
+ var variable1=2;
+    var  variable3= 15.9;
+    (variable1*variable3);
+ }
