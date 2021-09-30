d10ccb156fe32f9057e3a43267602f4e3cb3dc05
Jorge Ruiz <201914@CALASANZSALAMANCA.LOCAL>
2021-09-30 14:04:46 +0200

    add

---
+++ Desarrollo web en entorno clientge/empezar con js/4/js/js.js
@@ -0,0 +1,26 @@
+/* 
+function uno(){
+    alert("hola fqr4qrrt");
+   }
+   function mostrarVariables(){
+    var  variable1=34;
+       alert('Variable1: ' + variable1); 
+     
+   }
+ 
+   function multi(){
+ var variable1=2;
+    var  variable3= 15.9;
+    (variable1*variable3);
+ } */
+ var var1 = "hola";
+ var var2 = "adios";
+ function compr (){
+   alert(var1==var2) 
+   alert(var1!=var2)
+   alert(var1<var2)
+   alert(var1>var2)
+   alert(var1<=var2)
+   alert(var1>=var2)
+
+ }
---
+++ Desarrollo web en entorno clientge/empezar con js/5/js/img/js.js
@@ -0,0 +1,6 @@
+function usu(){
+  var n1  = prompt("hoy es dia")
+  var n2 = prompt ("el mes")
+  var n3 = prompt("año")
+  document.write("hoy es dia "+n1+"mes"+n2+"año"+n3)
+}
